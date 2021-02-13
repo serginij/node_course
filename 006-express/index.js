0c9895d98d029c18d5e7b26b9f58968597134096
serginij <kotlizkiy@gmail.com>
2021-02-13 19:36:25 +0300

    feat: add post & put & delete routes

--- 006-express/index.js
+++ 006-express/index.js
@@ -39,6 +39,48 @@ app.get('/api/books/:id', (req, res) => {
   res.status(200).json(book);
 });
 
+app.post('/api/books', (req, res) => {
+  const book = req.body;
+  const { booksStore } = store;
+
+  const { valid, errors } = booksStore.validateBook(book);
+
+  if (!valid) {
+    res.status(400).json({ message: 'Invalid data format', errors });
+  } else {
+    const data = booksStore.createBook(book);
+
+    res.status(201).json(data);
+  }
+});
+
+app.put('/api/books/:id', (req, res) => {
+  const book = req.body;
+  const { id } = req.params;
+  const { booksStore } = store;
+
+  const { valid, errors } = booksStore.validateBook(book, true);
+
+  if (!valid) {
+    res.status(400).json({ message: 'Invalid data format', errors });
+  } else {
+    const data = booksStore.updateBook(id, book);
+    res.status(200).json(data);
+  }
+});
+
+app.delete('/api/books/:id', (req, res) => {
+  const { id } = req.params;
+  const { booksStore } = store;
+
+  if (!booksStore.books[id]) {
+    res.status(404).json({ message: 'book not found' });
+  } else {
+    booksStore.deleteBook(id);
+    res.status(200).json({ message: 'ok' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log('App available on http://localhost:3000');
 });
--- 006-express/store/books/books.js
+++ 006-express/store/books/books.js
@@ -34,10 +34,32 @@ const deleteBook = (id) => {
   delete books[id];
 };
 
+const validateBook = (book, checkId = false) => {
+  if (!book) return { valid: false, error: ['No data provided'] };
+
+  let valid = true;
+  const errors = [];
+  const keys = ['title', 'description', 'authors', 'favorite', 'fileCover', 'fileName'];
+
+  if (checkId) {
+    keys.push('id');
+  }
+
+  keys.forEach((key) => {
+    if (typeof book[key] !== 'string') {
+      errors.push(key);
+      valid = false;
+    }
+  });
+
+  return { valid, errors };
+};
+
 module.exports = {
   books,
   getBooks,
   createBook,
   updateBook,
   deleteBook,
+  validateBook,
 };
