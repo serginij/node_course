d14010f8880061ac53624d091c9378479cf3aef4
serginij <kotlizkiy@gmail.com>
2021-03-22 22:30:43 +0300

    refactor: fix auth middleware redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,7 @@
-// module.exports = (req, res, next) => {
-//   if (!req.user && !['/user/login', '/user/signup'].includes(req.path))
-//     res.redirect('/user/login');
-//   else next();
-// };
-
 module.exports = (req, res, next) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
     if (!['/user/login', '/user/signup'].includes(req.path))
-      res.redirect('user/login');
+      res.redirect('/user/login');
     else if (req.session) {
       req.session.returnTo = req.originalUrl || req.url;
     }
